ec2bc1cec170e5452b198e0570c368471727befa
tomthethird <tflopez3@gmail.com>
2022-07-19 15:48:31 +0800

    JavaScript Exercise 3

---
+++ QuizA3_Car.js
@@ -0,0 +1,11 @@
+let car = {
+    manufacturer: "Toyota",
+    model: "Hilux 2.4 G 4x2 M/T",
+    engine: "2755 cc Diesel",
+    body: "Truck"
+}
+
+document.getElementById("manufacturer").innerHTML = car.manufacturer;
+document.getElementById("model").innerHTML = car.model;
+document.getElementById("engine").innerHTML = car.engine;
+document.getElementById("body").innerHTML = car.body; 
